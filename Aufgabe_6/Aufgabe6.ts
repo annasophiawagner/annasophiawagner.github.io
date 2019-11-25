336fae5248e5949cedc9d045e1d4bc490f9578f5
annasophiawagner <anna_wagner9797@web.de>
2019-11-25 23:22:21 +0100

    git commit -m "change"

--- Aufgabe_6/Aufgabe6.ts
+++ Aufgabe_6/Aufgabe6.ts
@@ -24,41 +24,42 @@ var asien2008: number = 12954.7;
 var australien2008: number = 1993;
 var gesamt2008: number = (afrika2008 + südamerika2008 + europa2008 + nordamerika2008 + asien2008+ australien2008);
 
-var texteuropa1: string = "Nach Stand von 2018 beträgt die Emission von Europa ";
-var texteuropa2: string = "Relativ zur Gesamtemission der Welt verursacht Europa damit ";
-var vergleichzahleuropa: number = (europa2018 - europa2008);
-var vergleicheuropa1: string = "Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleicheuropa2: string = "Im Vergleich 2018 zu 2008 sind das in Europa ";
+var ValueTwoAfrika: number = (afrika2018 / gesamt2018) * 100
+var ValueThreeAfrika: number = (afrika2018 - afrika2008) * 100 / afrika2008
+var ValueFourAfrika: number = (afrika2018 - afrika2008)
 
-var textafrika1: string = "Nach Stand von 2018 beträgt die Emission von Afrika ";
-var textafrika2: string = "Relativ zur Gesamtemission der Welt verursacht Afrika damit ";
-var vergleichzahlafrika: number = (afrika2018-afrika2008);
-var vergleichafrika1: string = "Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleichafrika2: string = "Im Vergleich 2018 zu 2008 sind das in Afrika ";
+var ValueTwoSüdamerika: number = (südamerika2018 / gesamt2018) * 100
+var ValueThreeSüdamerika: number = (südamerika2018 - südamerika2008) * 100 / südamerika2008
+var ValueFourSüdamerika: number = (südamerika2018 - südamerika2008)
 
-var textsüdamerika1: string = "Nach Stand von 2018 beträgt die Emission von Südamerika ";
-var textsüdamerika2: string = "Relativ zur Gesamtemission der Welt verursacht Südamerika damit ";
-var vergleichzahlsüdamerika: number = (südamerika2018 - südamerika2008);
-var vergleichsüdamerika1: string = "Für Südamerika at sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleichsüdamerika2: string = "Im Vergleich 2018 zu 2008 sind das in Südamerika ";
+var ValueTwoEuropa: number = (europa2018 / gesamt2018) * 100
+var ValueThreeEuropa: number = (europa2018 - europa2008) * 100 / europa2008
+var ValueFourEuropa: number = (europa2018 - europa2008)
 
-var textnordamerika1: string = "Nach Stand von 2018 beträgt die Emission von Nordamerika ";
-var textnordamerika2: string = "Relativ zur Gesamtemission der Welt verursacht Südamerika damit ";
-var vergleichszahlnordamerika: number = (nordamerika2018 - nordamerika2008);
-var vergleichnordamerika1: string = "Für Nordamerika at sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleichnordamerika2: string = "Im Vergleich 2018 zu 2008 sind das in Nordamerika ";
+var ValueTwoNordamerika: number = (nordamerika2018/ gesamt2018) * 100
+var ValueThreeNordamerika: number = (nordamerika2018- nordamerika2008) * 100 / nordamerika2008
+var ValueFourNordamerika: number = (nordamerika2018 - nordamerika2008)
 
-var textasien1: string = "Nach Stand von 2018 beträgt die Emission von Asien ";
-var textasien2: string = "Relativ zur Gesamtemission der Welt verursacht Asien damit ";
-var vergleichszahlasien: number = (asien2018 - asien2008);
-var vergleichasien1: string = "Für Asien at sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleichasien2: string = "Im Vergleich 2018 zu 2008 sind das in Asien ";
+var ValueTwoAsien: number = (asien2018 / gesamt2018) * 100
+var ValueThreeAsien: number = (asien2018 - asien2008) * 100 / asien2008
+var ValueFourAsien: number = (asien2018- asien2008)
 
-var textaustralien1: string = "Nach Stand von 2018 beträgt die Emission von Australien ";
-var textaustralien2: string = "Relativ zur Gesamtemission der Welt verursacht Australien damit ";
-var vergleichszahlaustralien: number = (australien2018 - australien2008);
-var vergleichaustralien1: string = "Für Australien at sich 2018 im Vergleich zu 2008 die Emission um ";
-var vergleichaustralien2: string = "Im Vergleich 2018 zu 2008 sind das in Australien ";
+var ValueTwoAustralien: number = (australien2018 / gesamt2018) * 100
+var ValueThreeAustralien: number = (australien2018- australien2008) * 100 / australien2008
+var ValueFourAustralien: number = (australien2018 - australien2008)
 
+function functioneuropa () {
+    document.querySelector("#Title").innerHTML = "Carbon Dioxide Emissions in Europe";
+    document.querySelector("#ValueOneEUROPATEXT").innerHTML = "Emission absolute of Europe in 2018";
+    document.querySelector("#ValueOne").innerHTML = "" + europa2018;
+    document.querySelector("#ValueTwo").innerHTML = "" + ValueTwoEuropa;
+    document.querySelector("#ValueThree").innerHTML = "" + ValueThreeEuropa;
+    document.querySelector("#ValueFour").innerHTML = "" + ValueFourEuropa;
+    document.querySelector(".chart").setAttribute('style', 'height:' + ValueTwoEuropa + '%');
+}
+
+window.addEventListener('load', function () {
+    document.querySelector('#europa').addEventListener('click', functioneuropa);
+});
 
 
