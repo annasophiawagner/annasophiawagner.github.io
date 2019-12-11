3469ba3f576578444cab3fd383bee2b421cf01b2
annasophiawagner <anna_wagner9797@web.de>
2019-12-11 15:11:17 +0100

    git commit -m "play8"

--- Aufgabe8/aufgabe8.js
+++ Aufgabe8/aufgabe8.js
@@ -1,4 +1,56 @@
-/*Record*/
+/*Sample PLay*/
+var Kick;
+var Beat = ["hihat.mp3", "snare.mp3", "C.mp3", "snare.mp3", "kick.mp3", "snare.mp3"];
+var music = false;
+function playSample(beat) {
+    var sound = new Audio("assets/" + beat);
+    sound.play();
+}
+window.addEventListener("load", function (beat) {
+    document.querySelector(".icon-play-circled").addEventListener("click", playsound);
+});
+function playsound() {
+    var mybeat = ["assets/laugh-2.mp3", "assets/kick.mp3", "assets/kick.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/hihat.mp3", "assets/F.mp3", "assets/A.mp3", "assets/F.mp3", "assets/A.mp3", "assets/F.mp3", "assets/C.mp3", "assets/hihat.mp3"];
+    var index = 0;
+    var interval = setInterval(mySound, 300);
+    function mySound() {
+        var MyMelody = new Audio(mybeat[index]);
+        MyMelody.play();
+        index += 1;
+        if (index > 11)
+            index = 0;
+        console.log(mybeat[index]);
+    }
+}
+function playBeat() {
+    var index = 0;
+    /*start*/
+    if (document.getElementById("play").classList.contains("fa-play")) {
+        document.getElementById("play").classList.remove("fa-play");
+        document.getElementById("play").classList.add("fa-pause");
+        Kick = setInterval(myBeat, 300);
+        music = false;
+    }
+    /*stop*/
+    else {
+        document.getElementById("play").classList.remove("fa-pause");
+        document.getElementById("play").classList.add("fa-play");
+        clearInterval(Kick);
+    }
+    /*beat*/
+    function myBeat() {
+        playSample(Beat[index]);
+        index += 1;
+        if (index > (Beat.length - 1))
+            index = 0;
+    }
+}
+function recordBeat() {
+    music = true;
+}
+function deleteBeat() {
+    Beat.length = 0;
+}
 /*Buttons*/
 window.addEventListener("load", function (beat) {
     document.querySelector("#ButtonOne").addEventListener("mousedown", function () { playSample("F.mp3"); });
@@ -26,14 +78,5 @@ window.addEventListener("load", function (beat) {
 });
 window.addEventListener("load", function (beat) {
     document.querySelector("#ButtonNine").addEventListener("mousedown", function () { playSample("hihat.mp3"); });
-});
-window.addEventListener("load", function (beat) {
-    document.querySelector("#play").addEventListener("click", icon - play - circled);
-});
-window.addEventListener("load", function (beat) {
-    document.querySelector("#stop").addEventListener("click", icon - trash);
-});
-window.addEventListener("load", function (beat) {
-    document.querySelector("#record").addEventListener("click", icon - mic);
 });
 //# sourceMappingURL=aufgabe8.js.map
