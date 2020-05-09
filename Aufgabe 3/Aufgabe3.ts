f9a8171580cafc5ef56a5f4d27518fd66b5fa21a
annasophiawagner <anna_wagner9797@web.de>
2020-05-09 17:39:32 +0200

    git commit -m "aufgabe3"

--- Aufgabe 3/Aufgabe3.ts
+++ Aufgabe 3/Aufgabe3.ts
@@ -5,7 +5,6 @@ namespace L03_FormElements {
         console.log("Init");
         let fieldsets: NodeListOf<HTMLFieldSetElement> = document.querySelectorAll("fieldset");
 
-        // Install listeners on fieldsets
         for (let i: number = 0; i < fieldsets.length; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", handleChange);
@@ -24,23 +23,5 @@ namespace L03_FormElements {
         // Handling checkbox
         if (target.type == "checkbox")
             console.log("Checked: " + target.name + " = " + target.checked);
-
-        // Slider response
-        // if (target.name == "Slider") {
-        //     let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("progress")[0];
-        //     progress.value = parseFloat(target.value);
-        // }
-
-        // Meter response
-        // if (target.name == "Stepper") {
-        //     let meter: HTMLMeterElement = <HTMLMeterElement>document.querySelector("meter");
-        //     meter.value = parseFloat(target.value);
-        // }
-
-        // Color response
-        // if (target.name == "Color") {
-        //     let ouput: HTMLOutputElement = <HTMLOutputElement>document.querySelector("output");
-        //     ouput.value = target.value;
-        // }
+        }
     }
-}
